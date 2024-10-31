5b5c51d31952284ce8252b3c7cc56fee0cbc7345
Mildred1999 <nwachukwu.mildred10@gmail.com>
2024-10-31 11:59:04 -0400

    Edited tables and updated erd

--- data_src/mercury_corp.sql
+++ data_src/mercury_corp.sql
@@ -11,6 +11,14 @@ dept_id INT NOT NULL AUTO_INCREMENT,
 dept_name varchar(45),
 PRIMARY KEY (dept_id)
 );
+CREATE TABLE IF NOT EXISTS medical_records(
+rec_id INT NOT NULL AUTO_INCREMENT,
+medications varchar(100),
+vaccinations varchar(100),
+allergies varchar(100),
+treatment varchar(100),
+PRIMARY KEY (rec_id)
+);
 CREATE TABLE IF NOT EXISTS employees(
 emp_id INT NOT NULL AUTO_INCREMENT,
 first_name varchar(100) NOT NULL,
@@ -43,8 +51,10 @@ emergency_calls varchar(100),
 hospice varchar(100),
 physician_id INT,
 note longtext,
+rec_id INT,
 PRIMARY KEY (note_id),
-FOREIGN KEY (physician_id) REFERENCES physician (physician_id)
+FOREIGN KEY (physician_id) REFERENCES physician (physician_id),
+FOREIGN KEY (rec_id) REFERENCES medical_records (rec_id)
 );
 CREATE TABLE IF NOT EXISTS accounting_record(
 account_id INT NOT NULL AUTO_INCREMENT,
@@ -62,16 +72,7 @@ primary key (visit_id),
 foreign key (physician_id) references physician (physician_id),
 foreign key (note_id) references clinical_notes(note_id)
 );
-CREATE TABLE IF NOT EXISTS medical_records(
-rec_id INT NOT NULL AUTO_INCREMENT,
-medications varchar(100),
-vaccinations varchar(100),
-allergies varchar(100),
-note_id INT,
-treatment varchar(100),
-PRIMARY KEY (rec_id),
-FOREIGN KEY (note_id) REFERENCES clinical_notes (note_id)
-);
+
 CREATE TABLE IF NOT EXISTS residential_sector(
 unit_id INT NOT NULL AUTO_INCREMENT,
 unit varchar(100),
@@ -172,25 +173,26 @@ INSERT INTO CARE (notes) VALUES ('Likes to eat lunch at 12:00 noon.');
 INSERT INTO CARE (notes) VALUES ('Goes to bed at 8:00 pm with.');
 INSERT INTO Care (notes) VALUES ('Eats breakfast at 8:00 am.');
 
-INSERT INTO clinical_notes
-(current_illness,medication_refusal,diet_plan, emergency_calls, physician_id, note)
+INSERT INTO medical_records
+(medications, vaccinations, allergies, treatment)
 VALUES
-('Peripheral Artery Disease', 'none','mediterranean diet', 'none',1, 'needs checked on regulary');
-
-INSERT INTO clinical_notes
-(current_illness,medication_refusal,diet_plan, emergency_calls, physician_id, note)
-VALUES
-('High Blood Pressure', 'none','grains', 'none',2, 'needs checked on regulary');
+('Amlodipine', 'Flu', 'Nut','XYZ');
 
 INSERT INTO medical_records
-(medications, vaccinations, allergies, note_id, treatment)
+(medications, vaccinations, allergies,  treatment)
 VALUES
-('Amlodipine', 'Flu', 'Nut', 1, 'XYZ');
+('Amlodipine', 'HPV', 'Milk',  'SAZ');
 
-INSERT INTO medical_records
-(medications, vaccinations, allergies, note_id, treatment)
+INSERT INTO clinical_notes
+(current_illness,medication_refusal,diet_plan, emergency_calls, physician_id, note, rec_id)
 VALUES
-('Amlodipine', 'HPV', 'Milk', 2, 'SAZ');
+('Peripheral Artery Disease', 'none','mediterranean diet', 'none',1, 'needs checked on regulary', 1);
+
+INSERT INTO clinical_notes
+(current_illness,medication_refusal,diet_plan, emergency_calls, physician_id, note, rec_id)
+VALUES
+('High Blood Pressure', 'none','grains', 'none',2, 'needs checked on regulary', 2);
+
 
 INSERT INTO residents 
 (first_name, last_name, dob, mailing_address, physician_id,account_id, ssn, emergency_contact, care_id, rec_id, unit_id, address)
