2105aadf553a3f13e11bab61c2a576f19e1d92ac
Mildred1999 <nwachukwu.mildred10@gmail.com>
2024-11-04 00:31:58 -0500

    more edits to login

--- web_src/login.php
+++ web_src/login.php
@@ -66,6 +66,35 @@
       </div>
     
 </body>
+
+<?php
+include('../includes/config.php');
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
+    $stmt->execute([$username]);
+    $user = $stmt->fetch();
+
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['user'] = $user['username'];
+        $_SESSION['role'] = $user['role'];
+
+        if ($user['role'] === 'nurse') {
+            header("Location: /views/nurse-dashboard.php");
+        } elseif ($user['role'] === 'resident') {
+            header("Location: /views/resident-dashboard.php");
+        }
+        exit;
+    } else {
+        $error = "Invalid username or password.";
+    }
+}
+?>
+
 <footer>
   <p> 2024 Mercury Corp. All rights reserved.</p>
   <p>Follow us on social media!</p>
