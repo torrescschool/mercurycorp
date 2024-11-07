72906ffe92b879e0f3ae4b476bf5cff9b6bd42f1
Laneyeh <humblel@etown.edu>
2024-11-07 10:19:14 -0500

    made a lot of changes to the residents page

--- web_src/main.js
+++ web_src/main.js
@@ -26,3 +26,9 @@ function init(){
         })
     }
 }
+
+function toggleInfo(id) {
+    const info = document.getElementById(id);
+    info.style.display = info.style.display === 'none' || info.style.display === '' ? 'block' : 'none';
+}
+
