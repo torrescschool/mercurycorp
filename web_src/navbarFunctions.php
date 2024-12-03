f1dc0149663138b7be0c3d1a1deffaf78f25253a
Mildred1999 <nwachukwu.mildred10@gmail.com>
2024-12-02 22:31:49 -0500

    edited navber functions

--- web_src/navbarFunctions.php
+++ web_src/navbarFunctions.php
@@ -5,7 +5,7 @@ function url(){
     if($_SERVER["HTTP_HOST"]=="127.0.0.1" || $_SERVER["HTTP_HOST"]=="localhost")
         return "http://".$_SERVER["HTTP_HOST"]."/mercurycorp/web_src/";
     else
-        return "https://".$_SERVER["HTTP_HOST"]."/web_src/";
+        return "https://".$_SERVER["HTTP_HOST"]."/mercurycorp/web_src/";
 }
 
 
