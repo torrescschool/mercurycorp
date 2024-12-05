97d7672564e269fb511a2f70944e065b443e18ad
Laneyeh <humblel@etown.edu>
2024-12-05 09:58:01 -0500

    added to footer.php

--- web_src/footer.php
+++ web_src/footer.php
@@ -2,12 +2,12 @@
         <p>2024 Mercury Corp. All rights reserved.</p>
         <p>Follow us on social media!</p>
         <a href="https://github.com/Laneyeh">
-          <img class="socialMediaIcon" src="<?php echo url();?>photos/facebook.png" alt="Facebook">
+          <img class="socialMediaIcon" src="photos/facebook.png" alt="Facebook">
         </a>
         <a href="https://github.com/torrescschool">
-          <img class="socialMediaIcon" src="<?php echo url();?>photos/instagram.png" alt="Instagram">
+          <img class="socialMediaIcon" src="photos/instagram.png" alt="Instagram">
         </a>
         <a href="https://github.com/Mildred1999">
-          <img class="socialMediaIcon" src="<?php echo url();?>photos/twitter.png" alt="Twitter">
+          <img class="socialMediaIcon" src="photos/twitter.png" alt="Twitter">
         </a>
       </footer>
